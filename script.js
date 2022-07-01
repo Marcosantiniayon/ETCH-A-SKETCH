fc78870258a2f6036131d8961975b4fc0774a3d4
Marco S <marcosantinia12@gmail.com>
2022-06-30 17:59:58 -0700

    Changed sizing of grid container, added button, changed layout, cleaned up code

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("cell");
+let pixelButton = document.getElementsByClassName("pixel");
 
 // Creates a default grid sized 16x16
 function defaultGrid() {
@@ -13,9 +14,9 @@ function defaultGrid() {
 
 // Takes (rows, columns) input and makes a grid
 function makeRows(rowNum) {
-
+    rowNumb = rowNum-1;
     // Creates rows
-    for (r = 0; r < rowNum; r++) {
+    for (r = 0; r <= rowNumb; r++) {
         let row = document.createElement("div");
         container.appendChild(row).className = "gridRow";
     };
@@ -23,21 +24,16 @@ function makeRows(rowNum) {
 
 // Creates columns
 function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
+    cellNumb = cellNum-1;
+    let rowsLength = rows.length-1;
+    for (i = 0; i <= rowsLength; i++) {
+        for (j = 0; j <= cellNumb; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cell";
         };
 
     };
 };
-
-// Hover effect
-// for(i = 0; i < cells.length; i++){
-//     cells[i].addEventListener("mouseenter", () => {
-//     cells[i].classList.add("hover");
-//     })
-// }
 
 //function that changes div color uponed being hovered
 function hoverColor() {
@@ -48,5 +44,7 @@ function hoverColor() {
       });
     });
   }
+
+//function that prompts user to change # of pixels which changes the # of grids  
 
 defaultGrid();
